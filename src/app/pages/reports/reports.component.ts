14fa2147fd2d62b3fe459e55d40893db41bde815
Matheus Alves de Amorim Santos <matheus.santos@nava.com.br>
2021-12-07 14:05:16 -0300

    final commit

---
+++ src/app/pages/reports/reports.component.ts
@@ -0,0 +1,93 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PoNotificationService } from '@po-ui/ng-components';
+import { TicketsService } from 'src/app/services/tickets.service';
+
+@Component({
+  selector: 'app-reports',
+  templateUrl: './reports.component.html',
+  styleUrls: ['./reports.component.scss']
+})
+export class ReportsComponent implements OnInit {
+
+  public idReport: number = 0;
+
+  public headers: string[] = [];
+  public displayedColumns: string[] = [];
+  public dataSource: any[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private notification: PoNotificationService,
+    private ticketsService: TicketsService
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(data => {
+      this.idReport = parseInt(data.id, 10);
+      this.getNotification(this.idReport);
+    })
+  }
+
+  private getNotification(idReport: number) {
+    if (idReport === 1) {
+      this.ticketsService
+        .findAllOrder()
+        .subscribe(data => {
+          this.dataSource = data.map(map => {
+            const date = map.paymentMoment?.toString() as string;
+            return {
+              id: map.id,
+              price: `R$ ${map.price?.toLocaleString('pt-BR', { currency: 'BRL' })}` as any,
+              quantity: map.quantity,
+              product: map.product?.name as any,
+              paymentMoment: `${date.substr(8, 2)}/${date.substr(5, 2)}/${date.substr(0, 4)} ${date.substr(11, 5)}` as any,
+              total: `R$ ${map.total?.toLocaleString('pt-BR', { currency: 'BRL' })}` as any
+            }
+          });
+          this.displayedColumns = ['id', 'price', 'quantity', 'product', 'paymentMoment', 'total'];
+          this.headers = ['Id', 'Preço', 'QTD Compra', 'Show', 'Data do Pagamento', 'Valor Total'];
+        },
+          (error: HttpErrorResponse) => {
+            this.notification.error('Erro ao carregar dados do relatório');
+            console.log(error.error);
+          })
+    } else if (idReport == 2) {
+      this.ticketsService
+        .findDashboard()
+        .subscribe(data => {
+          this.dataSource = data.map(map => {
+            const date = map.dateShow.toString();
+            map.dateShow = `${date.substr(8, 2)}/${date.substr(5, 2)}/${date.substr(0, 4)} ${date.substr(11, 5)}` as any;
+            map.totalValue = `R$ ${map.totalValue.toLocaleString('pt-BR', { currency: 'BRL' })}` as any;
+            return map;
+          });
+          this.displayedColumns = ['name', 'address', 'dateShow', 'totalTickets', 'totalValue'];
+          this.headers = ['Nome', 'Endereço', 'Data do Show', 'QTD. Total de ingressos', 'Valor Total'];
+        },
+          (error: HttpErrorResponse) => {
+            this.notification.error('Erro ao carregar dados do relatório');
+            console.log(error.error);
+          })
+    } else if (idReport === 3) {
+      this.ticketsService
+        .findDashboardAddress()
+        .subscribe(data => {
+          this.dataSource = data.map(map => {
+            map.totalValue = `R$ ${map.totalValue.toLocaleString('pt-BR', { currency: 'BRL' })}` as any;
+            return map;
+          });
+          this.displayedColumns = ['address', 'totalShow', 'totalOrder', 'totalTickets', 'totalValue'];
+          this.headers = ['Cidade', 'Total de shows', 'Total da compra', 'QTD. total de ingressos', 'Valor Total'];
+        },
+          (error: HttpErrorResponse) => {
+            this.notification.error('Erro ao carregar dados do relatório');
+            console.log(error.error);
+          })
+    } else {
+      this.notification.warning('Erro ao carregar o relatório');
+    }
+  }
+
+}
